e2f7e2d70e6e216c4c84dee7a710be4cc47aff53
AbidKhairyAK <abid.khairy.ak@gmail.com>
2018-05-21 10:39:54 +0700

    Display all posts to the blog index

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+	public function getImageUrlAttribute($value)
+	{
+		$imageUrl = "";
+
+		if (! is_null($this->image))
+		{
+			$imagePath = public_path()."/AbidBlog/img/".$this->image;
+			if (file_exists($imagePath)) $imageUrl = asset("AbidBlog/img/".$this->image);
+		}
+
+		return $imageUrl;
+	}
 }
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -8,12 +8,18 @@
 		<!-- Content -->
 		<div id="content" class="col-md-8">
 
+			@foreach($posts as $post)
+			
 	      <div class="thumbnail">
 	        <a href="#" target="_blank">
-	          <img src="/AbidBlog/img/Post_Image_2.jpg" alt="Lights" class="img-responsive">
+
+	        	@if ($post->imageUrl)
+	          	<img src="{{ $post->image_url }}" alt="Post Image" class="img-responsive">
+	          @endif
+	          
 	          <div class="caption">
-	          	<h2>Lorem ipsum donec id elit non donec id elit non mi porta</h2>
-	            <p class="text-justify">Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec porta.</p>
+	          	<h2>{{ $post->title }}</h2>
+	            <p class="text-justify">{{ $post->excerpt }}</p>
 	          </div>
 	        </a>
 	        <div class="panel panel-default">
@@ -27,24 +33,7 @@
 	        </div>
 	      </div>
 
-	      <div class="thumbnail">
-	        <a href="#" target="_blank">
-	          <img src="/AbidBlog/img/Post_Image_3.jpg" alt="Lights" class="img-responsive">
-	          <div class="caption">
-	          	<h2>Lorem ipsum donec id elit non donec id elit non mi porta</h2>
-	            <p class="text-justify">Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec id elit non mi porta gravida at eget metus. Lorem ipsum donec porta.</p>
-	          </div>
-	        </a>
-	        <div class="panel panel-default">
-	          <ul class="panel-body list-inline">
-	          	<li><i class="fa fa-user"></i> <span class="text-muted">Admin</span></li>
-	          	<li><i class="fa fa-clock-o"></i> February, 12 2016</li>
-	          	<li><i class="fa fa-tags"></i> <span class="text-muted">Blog</span></li>
-	          	<li><i class="fa fa-comments"></i> <span class="text-muted">4 Comments</span></li>
-	          	<li class="pull-right text-muted"><a href="#" target="_blank">Continue Reading &raquo;</a></li>
-	          </ul>
-	        </div>
-	      </div>
+      @endforeach
 
 		  <!-- Pager -->
 		  <ul class="pager">
